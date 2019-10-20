890007920f411f74e85c00c078d48f3f6ec2323e
Linden Daniels <lindendaniels@gmail.com>
2019-10-19 21:10:22 -0400

    Fix genre

--- app.js
+++ app.js
@@ -32,19 +32,13 @@ app.get('/apps', (req, res) => {
           return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
       });
     }
-
-    
-
     if (Genres) {
       if(!['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'].includes(Genres)) {
           return res.status(400).send('Genre must be action, puzzle, strategy, casual, arcade, or card.')
       }
-      let genre = playapps.filter(playapp => playapp.Genres.toLowerCase().includes(Genres.toLowerCase()));
-      console.log(genre);
+      results = playapps.filter(playapp => playapp.Genres.toLowerCase().includes(Genres.toLowerCase()));
+     
   }
-
-  
-    
     res
       .json(results);
   });
