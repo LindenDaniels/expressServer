31e8bde58038ba7dca7c1efefaf5685026532e3b
Linden Daniels <lindendaniels@gmail.com>
2019-10-20 14:12:24 -0400

    Implement tests

--- test/app.test.js
+++ test/app.test.js
@@ -14,6 +14,10 @@ describe('GET /apps', () => {
         return supertest(app).get('/apps').query({ sort: 'MISTAKE' }).expect(400, 'Sort must be rating or app')
     })
 
+    it('should be 400 if Genre is incorrect', () => {
+        return supertest(app).get('/apps').query({ Genres: 'MISTAKE' }).expect(400, 'Genre must be action, puzzle, strategy, casual, arcade, or card.')
+    })
+
     it('should sort by rating', () => {
         return supertest(app)
         .get('/apps')
@@ -31,6 +35,32 @@ describe('GET /apps', () => {
             const appAtIPlus1 = res.body[i + 1];
 
         if (appAtIPlus1.Rating < appAtI.Rating) {
+            sorted = false;
+            break;
+        }
+        i++
+        }
+        expect(sorted).to.be.true;
+        });
+    });
+
+    it('should sort by App', () => {
+        return supertest(app)
+        .get('/apps')
+        .query({ sort: 'App' })
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .then(res => {
+            expect(res.body).to.be.an('array');
+        let sorted = true;
+
+        let i = 0;
+
+        while (i < res.body.length - 1) {
+            const appAtI = res.body[i];
+            const appAtIPlus1 = res.body[i + 1];
+
+        if (appAtIPlus1.App < appAtI.App) {
             sorted = false;
             break;
         }
